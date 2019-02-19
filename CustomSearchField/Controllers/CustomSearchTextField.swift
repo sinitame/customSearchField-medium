46af0464f1331b8d14755e848458ce716406d7a1
Emrick Sinitambirivoutin <emrick.sinitambirivoutin@grenoble-inp.org>
2019-02-19 17:21:00 +0100

    Setting up the MVC hierarchy and new class file

---
+++ CustomSearchField/Controllers/CustomSearchTextField.swift
@@ -0,0 +1,15 @@
+//
+//  CustomSearchTextField.swift
+//  CustomSearchField
+//
+//  Created by Emrick Sinitambirivoutin on 19/02/2019.
+//  Copyright © 2019 Emrick Sinitambirivoutin. All rights reserved.
+//
+
+import UIKit
+
+class CustomSearchTextField: UITextField {
+
+    //Here is where we will configure our custom UITextField class
+
+}
