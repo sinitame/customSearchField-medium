21d797596c575e915230b1752ed930c1f628b105
Emrick Sinitambirivoutin <emrick.sinitambirivoutin@grenoble-inp.org>
2019-02-19 22:33:31 +0100

    Addition of the tableView that handles autocompletion results display

--- CustomSearchField/Controllers/CustomSearchTextField.swift
+++ CustomSearchField/Controllers/CustomSearchTextField.swift
@@ -8,8 +8,164 @@
 
 import UIKit
 
-class CustomSearchTextField: UITextField {
+class CustomSearchTextField: UITextField{
+    
+    var dataList : [String] = []
+    var tableView: UITableView?
+    
+    
+    
+    // Connecting the new element to the parent view
+    open override func willMove(toWindow newWindow: UIWindow?) {
+        super.willMove(toWindow: newWindow)
+        tableView?.removeFromSuperview()
+        
+    }
+    
+    override open func willMove(toSuperview newSuperview: UIView?) {
+        super.willMove(toSuperview: newSuperview)
+        
+        self.addTarget(self, action: #selector(CustomSearchTextField.textFieldDidChange), for: .editingChanged)
+        self.addTarget(self, action: #selector(CustomSearchTextField.textFieldDidBeginEditing), for: .editingDidBegin)
+        self.addTarget(self, action: #selector(CustomSearchTextField.textFieldDidEndEditing), for: .editingDidEnd)
+        self.addTarget(self, action: #selector(CustomSearchTextField.textFieldDidEndEditingOnExit), for: .editingDidEndOnExit)
+    }
+    
+    
+    override open func layoutSubviews() {
+        super.layoutSubviews()
+        buildSearchTableView()
+        
+    }
+    
+    
+    //////////////////////////////////////////////////////////////////////////////
+    // Text Field related methods
+    //////////////////////////////////////////////////////////////////////////////
+    
+    @objc open func textFieldDidChange(){
+        print("Text changed ...")
+        addData()
+        updateSearchTableView()
+        tableView?.isHidden = false
+    }
+    
+    @objc open func textFieldDidBeginEditing() {
+        print("Begin Editing")
+    }
+    
+    @objc open func textFieldDidEndEditing() {
+        print("End editing")
 
-    //Here is where we will configure our custom UITextField class
+    }
+    
+    @objc open func textFieldDidEndEditingOnExit() {
+        print("End on Exit")
+    }
+    
 
+}
+
+extension CustomSearchTextField: UITableViewDelegate, UITableViewDataSource {
+    
+
+    //////////////////////////////////////////////////////////////////////////////
+    // Table View related methods
+    //////////////////////////////////////////////////////////////////////////////
+    
+    
+    // MARK: TableView creation and updating
+    
+    // Create SearchTableview
+    func buildSearchTableView() {
+        
+        if let tableView = tableView {
+            tableView.register(UITableViewCell.self, forCellReuseIdentifier: "CustomSearchTextFieldCell")
+            tableView.delegate = self
+            tableView.dataSource = self
+            self.window?.addSubview(tableView)
+
+        } else {
+            print("tableView created")
+            tableView = UITableView(frame: CGRect.zero)
+        }
+        
+        updateSearchTableView()
+    }
+    
+    // Updating SearchtableView
+    func updateSearchTableView() {
+        
+        if let tableView = tableView {
+            superview?.bringSubviewToFront(tableView)
+            var tableHeight: CGFloat = 0
+            tableHeight = tableView.contentSize.height
+            
+            // Set a bottom margin of 10p
+            if tableHeight < tableView.contentSize.height {
+                tableHeight -= 10
+            }
+            
+            // Set tableView frame
+            var tableViewFrame = CGRect(x: 0, y: 0, width: frame.size.width - 4, height: tableHeight)
+            tableViewFrame.origin = self.convert(tableViewFrame.origin, to: nil)
+            tableViewFrame.origin.x += 2
+            tableViewFrame.origin.y += frame.size.height + 2
+            UIView.animate(withDuration: 0.2, animations: { [weak self] in
+                self?.tableView?.frame = tableViewFrame
+            })
+            
+            //Setting tableView style
+            tableView.layer.masksToBounds = true
+            tableView.separatorInset = UIEdgeInsets.zero
+            tableView.layer.cornerRadius = 5.0
+            tableView.separatorColor = UIColor.lightGray
+            tableView.backgroundColor = UIColor.white.withAlphaComponent(0.4)
+            
+            if self.isFirstResponder {
+                superview?.bringSubviewToFront(self)
+            }
+            
+            tableView.reloadData()
+        }
+    }
+    
+    
+    
+    // MARK: TableViewDataSource methods
+    public func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        print(dataList.count)
+        return dataList.count
+    }
+    
+    // MARK: TableViewDelegate methods
+    
+    //Adding rows in the tableview with the data from dataList
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CustomSearchTextFieldCell", for: indexPath) as UITableViewCell
+        cell.backgroundColor = UIColor.clear
+        cell.textLabel?.text = dataList[indexPath.row]
+        return cell
+    }
+    
+    public func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("selected row")
+        self.text = dataList[indexPath.row]
+        tableView.isHidden = true
+        self.endEditing(true)
+    }
+    
+
+    // MARK: Early testing methods
+    func addData(){
+        print(self.text!)
+        dataList.append(self.text!)
+        print(dataList)
+    }
+    
 }
