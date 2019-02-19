897b9825dce363e069baa63d466942bfcbd030e7
Emrick Sinitambirivoutin <emrick.sinitambirivoutin@grenoble-inp.org>
2019-02-19 16:24:53 +0100

    Initial Commit

---
+++ CustomSearchField/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CustomSearchField
+//
+//  Created by Emrick Sinitambirivoutin on 19/02/2019.
+//  Copyright © 2019 Emrick Sinitambirivoutin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
